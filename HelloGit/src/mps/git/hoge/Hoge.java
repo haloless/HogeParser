c907d0d3f07c84980db6d4993476460b30057db6
haloless <sunxiaosong231@gmail.com>
2011-07-17 12:43:16 +0900

    hoge

---
+++ HelloGit/src/mps/git/hoge/Hoge.java
@@ -0,0 +1,13 @@
+package mps.git.hoge;
+
+public class Hoge {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hoge.main()");
+	}
+
+}
